3c4131753068f750388c8db46c529e5e1ff5fcca
yopadd <yoann.bourdex@gmail.com>
2018-03-01 19:22:28 +0100

    read data

--- index.js
+++ index.js
@@ -5,6 +5,21 @@ main()
 exec('zip -r ./out/source.zip index.js package.json test src')
 
 async function main () {
+  await example()
+}
+
+async function example () {
   const file = await readIn('example.in')
-  await writeOut('example.out', file)
+  const [head, ...lines] = file.trim().split('\n')
+  const [R, C, F, N, B, T] = head.split(' ')
+  const data = lines.map(line => {
+    const arr = line.split(' ')
+    return {
+      rideFrom: arr.slice(0, 2),
+      rideTo: arr.slice(2, 4),
+      earliestStart: arr[4],
+      latestFinish: arr[5]
+    }
+  })
+  console.log(data)
 }
